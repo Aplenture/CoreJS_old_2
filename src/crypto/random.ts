e18764ea6a32666fc1d26323c1b66eee08d181df
fweise <public@weise-games.de>
2023-09-05 15:46:45 +0200

    use SplitMix32 as PRNG instead of mulberry32 because of better distributed numbers

--- src/crypto/random.ts
+++ src/crypto/random.ts
@@ -46,14 +46,14 @@ export const randomHex = (function () {
 })();
 
 /*!
- * mulberry32 seeded pseudo random function
- * https://github.com/bryc/code/blob/master/jshash/PRNGs.md#mulberry32
+ * SplitMix32 seeded pseudo random function
+ * https://github.com/bryc/code/blob/master/jshash/PRNGs.md#splitmix32
  */
 export function randomSeeded(seed: number): number {
-    seed |= 0; seed = seed + 0x6D2B79F5 | 0;
-    let t = Math.imul(seed ^ seed >>> 15, 1 | seed);
-    t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    seed |= 0; seed = seed + 0x9e3779b9 | 0;
+    var t = seed ^ seed >>> 15; t = Math.imul(t, 0x85ebca6b);
+        t = t ^ t >>> 13; t = Math.imul(t, 0xc2b2ae35);
+    return ((t = t ^ t >>> 16) >>> 0) / 4294967296;
 }
 
 export function randomRanged(max: number, min = 0, seed = Number(random(4))): number {
